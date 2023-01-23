941ec72fa35966bd627b5154fa62c69413f04c67
ninajigania <ninajigania@gmail.com>
2023-01-24 03:38:58 +0400

    update

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("Hello world");
+console.log("Hello World");
+let user="Nina";
+let age=21;
+let isFemale = true;
+let car = null;
+let surName = "Jigania";
+
+let Fullname = (`${user} ${surName}`)
+console.log(Fullname);
+
+console.log(typeof isFemale);
+
+let x = 30;
+let y = 7;
+
+let sum = x + y;
+console.log(sum);
+
+let div = x / y
+console.log(div);
+
+let balance= x % y;
+console.log(balance);
+
+let sub = x - y;
+console.log (sub);
+
+let multi = x * y;
+console.log (multi);
+
+let radius = 7;
+console.log(Math.PI);
+
+const PI = 3.14;
+
+let circle = PI * radius * radius ;
+console.log(circle);
+
+let a = 7;
+let b = 5;
+let area = a * b;
+console.log(area);
